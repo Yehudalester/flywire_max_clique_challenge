f8a1c786aee5e5488d1573c03d205c1fcc880d77
Yehudalester <yehudalester1234@gmail.com>
2025-06-10 23:37:59 +0300

    added the files

---
+++ try.cpp
@@ -0,0 +1,82 @@
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<vector>
+#include<map>
+#include<queue>
+#include<random>
+#include<algorithm>
+using namespace std;
+using ll=long long;
+map<ll,vector<ll>> g;
+map<pair<ll,ll>,bool> adj_m;
+map<ll,ll> deg;
+vector<ll> nodes;
+inline ll f(ll n){
+    if(n<=4)return 2;
+    if(n<=50)return 4;
+    if(n<=100)return 6;
+    return 8;
+}
+vector<ll> attempt(ll x){
+    queue<ll> q;
+    q.push(x);
+    vector<ll> clique;
+    ll edge_count=0;
+    map<ll,bool> vis;
+    vis[x]=1;
+    while(!q.empty()){
+        ll a=q.front();q.pop();
+        ll e=0;
+        for(ll node:clique){
+            e+=adj_m[{a,node}];
+        }
+        if(e+edge_count<clique.size()*(clique.size()+1)/f(clique.size()))continue;
+        edge_count+=e;
+        clique.push_back(a);
+        for(ll nei:g[a])if(!vis[nei]){vis[nei]=1;q.push(nei);}
+    }
+    return clique;
+}
+int main(){
+    ll best=285;
+    ifstream edges("edges.csv");
+    string str;
+    while(getline(edges,str)){
+        ll a=0,b=0,i=0,f=0;
+        while(i<str.length()){
+            if(str[i]==','){f=1;}
+            else if(f==0){
+                a*=10;
+                a+=str[i]-'0';
+            }
+            else{
+                b*=10;
+                b+=str[i]-'0';
+            }
+            i++;
+        }
+        g[a].push_back(b);
+        g[b].push_back(a);
+        deg[a]++;deg[b]++;
+        adj_m[{a,b}]=adj_m[{b,a}]=1;
+    }
+    for(pair<ll,ll> p:deg){
+        nodes.push_back(p.first);
+    }
+    sort(nodes.begin(),nodes.end(), [&](ll a, ll b){return deg[a]<deg[b]||(deg[a]==deg[b]&&a<b);});
+    vector<ll> res;
+    vector<ll> vect;
+    for(ll x:nodes){
+        vect=attempt(x);
+        if(vect.size()>res.size()){swap(res,vect);cout << res.size() << " ";
+        if(res.size()>best){
+        ofstream result("result.csv");
+        for(ll x:res){
+            result << x <<"\n";
+        }
+        }
+    }
+    }
+    return 0;
+}
